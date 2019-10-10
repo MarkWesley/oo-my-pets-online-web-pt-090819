cece3fe0a1c4b523e4e8e8ff38fa59fbc85fb33a
Mark Stone <markwstone85@gmail.com>
2019-10-10 23:33:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,18 @@
 class Dog
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   attr_reader :name
 
+  @@all 
 
-  def initialize(name, owner)
+  def initialize(name, owner, mood = "nervous")
     @name = name
     @owner = owner
+    @mood = mood
+    @@all << self
+  end
+
+  def self.all 
+    @@all
   end
 
 end

4ac089365f9f5a946ee0557e0426fbbadd321494
Mark Stone <markwstone85@gmail.com>
2019-10-10 23:28:02 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,12 @@
 class Owner
   attr_reader :name, :species
 
+  @@all = []
+
   def initialize(name, species = "human")
     @name = name
     @species = species
+    @@all << self
   end
 
   def say_species
@@ -11,7 +14,8 @@ class Owner
   end
 
   def self.all
-
+    @@all 
+    
   end
 
 end
